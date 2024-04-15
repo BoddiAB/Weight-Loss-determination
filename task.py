ad225151f243a0a940c11395b23808771f052d09
BoddiAB <166543302+BoddiAB@users.noreply.github.com>
2024-04-16 00:58:04 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,10 @@
+
+# Get weight loss percentage with user input
+ini_wt = float(input("Enter the initial weight of sapota (in grams): "))
+fi_wt = float(input("Enter the final weight of sapoa (in grams): "))
+print("thank you for entering your values")
+# Calculate weight loss using the formula
+weight_loss_percentage = ((ini_wt - fi_wt) / ini_wt) * 100
+# Display weight loss
+print(f"The weight loss percentage of sapota is: {weight_loss_percentage:.2f}%")
+print("the wt percentage is :{}.".format(weight_loss_percentage))
